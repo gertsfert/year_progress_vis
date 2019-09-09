2f4882f4f5ac04a91d961af21e461aeebcc530c0
gertsfert <gertsfert@gmail.com>
2019-09-10 06:33:40 +1000

    Remove vis code from notebook

--- notebooks/text_parsing_test.py
+++ notebooks/text_parsing_test.py
@@ -116,48 +116,5 @@ df = df.drop([
 
 df.sample(20)
 
-# %% Visulising for the hell of it
-
-replies_fig = go.Scatter(
-    x=df.index,
-    y=df['replies'],
-    hovertext=df['year_perc'],
-    hoverinfo='text',
-    mode='lines',
-    name='Replies')
-
-likes_fig = go.Scatter(
-    x=df.index,
-    y=df['likes'],
-    hovertext=df['year_perc'],
-    hoverinfo='text',
-    mode='lines',
-    name='Likes')
-
-retweets_fig = go.Scatter(
-    x=df.index,
-    y=df['retweets'],
-    hovertext=df['year_perc'],
-    hoverinfo='text',
-    mode='lines',
-    name='Retweets')
-
-fig = make_subplots(
-    rows=3, cols=1,
-    vertical_spacing=0.1)
-
-fig.append_trace(replies_fig, 1, 1)
-fig.append_trace(likes_fig, 2, 1)
-fig.append_trace(retweets_fig, 3, 1)
-
-
-fig.update_layout(
-    height=900,
-    width=800,
-    title_text='Year Progress Twitter Account'
-)
-
-
-fig.show()
 
 # %%
